3dae589dd141ee6104a3640618b619dd5e98b06f
Telos11 <nestortleiro@gmail.com>
2025-11-20 19:39:22 +0100

    added new service, changed comps

---
+++ src/app/core/services/search-state.ts
@@ -0,0 +1,11 @@
+import { Injectable, signal } from '@angular/core';
+import { Track } from './music';
+
+@Injectable({ providedIn: 'root' })
+export class SearchStateService {
+  selectedTrack = signal<Track | null>(null);
+
+  setSelectedTrack(track: Track | null) {
+    this.selectedTrack.set(track);
+  }
+}
