52c27cbd91608b514d193301e15b0a846d42ca06
Telos11 <nestortleiro@gmail.com>
2026-02-08 22:08:37 +0100

    fix routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'trackDetail/:id',
-    renderMode: RenderMode.Server // Usamos SSR en lugar de Prerender para estas páginas dependientes de un id
-  },
-  {
-    path: 'albumDetail/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
