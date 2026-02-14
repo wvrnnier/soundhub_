13d5784cb21993505ded5a3b92ffcb69de7cb39d
Telos11 <nestortleiro@gmail.com>
2026-02-14 20:29:40 +0100

    added styles, animations and waiting behaviour to carrousel

--- src/app/shared/components/carrusel/carrusel.ts
+++ src/app/shared/components/carrusel/carrusel.ts
@@ -1,10 +1,11 @@
-import { Component, input, signal, computed } from '@angular/core';
+import { Component, input, signal, computed, effect, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackCardComponent } from '../track-card/track-card';
 import { AlbumComponent } from '../album/album';
 
 @Component({
   selector: 'app-carrusel',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
   imports: [CommonModule, TrackCardComponent, AlbumComponent],
   templateUrl: './carrusel.html',
@@ -19,6 +20,12 @@ export class CarruselComponent {
 
   itemsPerView = 5; //eligo numero de album en el carrusel
   page = signal(0);
+  animating = signal(false);
+
+  // Placeholders para shimmer
+  placeholders = Array(5).fill(0);
+
+  isLoading = computed(() => this.items().length === 0);
 
   totalPages = computed(() => Math.ceil(this.items().length / this.itemsPerView));
 
@@ -28,10 +35,21 @@ export class CarruselComponent {
   });
 
   next() {
-    if (this.page() < this.totalPages() - 1) this.page.update((p) => p + 1);
+    if (this.page() < this.totalPages() - 1) {
+      this.triggerAnimation();
+      this.page.update((p) => p + 1);
+    }
   }
 
   prev() {
-    if (this.page() > 0) this.page.update((p) => p - 1);
+    if (this.page() > 0) {
+      this.triggerAnimation();
+      this.page.update((p) => p - 1);
+    }
+  }
+
+  private triggerAnimation() {
+    this.animating.set(true);
+    setTimeout(() => this.animating.set(false), 400);
   }
 }
